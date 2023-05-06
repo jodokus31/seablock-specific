2afe3cfbd13c49cec6e04cf08cf19e4ec685a0c4
jodokus31 <33641866+jodokus31@users.noreply.github.com>
2023-05-06 17:11:10 +0200

    Fixed some issues hotkeys on same button

--- extended-fasttransfer/control.lua
+++ extended-fasttransfer/control.lua
@@ -1,5 +1,3 @@
-local TICKS_TO_DETECT_DRAG = 20
-
 local custom_inputs = require("custom_inputs")
 
 local chest = require("scripts/chest")
@@ -8,7 +6,9 @@ local assembler = require("scripts/assembler")
 local drop = require("scripts/drop")
 local partialstacks = require("scripts/partialstacks")
 
+local actiontype = require("scripts/actiontype")
 local flying_text = require("scripts/flying_text")
+
 local logger = require("scripts/logger")
 
 script.on_init(function()
@@ -21,7 +21,7 @@ function get_player_state(player_index)
 		state = 
 		{
 			last_action_tick = nil,
-			last_action_type = nil,
+			last_action_types = nil,
 			setting_custom_drop_amount = settings.get_player_settings(player_index)["extended-fasttransfer-custom-drop-amount"].value,
 			setting_max_ammo_amount    = settings.get_player_settings(player_index)["extended-fasttransfer-max-ammo-amount"].value,
 		}
@@ -30,94 +30,60 @@ function get_player_state(player_index)
 	return state
 end
 
-local function set_action(state, tick, type, force)
-	if not state.last_action_tick or force or state.last_action_tick < tick then
-		state.last_action_tick = tick
-		state.last_action_type = {}
-		state.last_action_type[type] = true
-		logger.print(player, "set_action: " .. (force and "(force) " or "") .. (type or "nil"))
-	elseif state.last_action_tick == tick then
-		state.last_action_type[type] = true
-		logger.print(player, "add_action: " .. (type or "nil"))
-	end
-end
-
-local function reset_action(state)
-	state.last_action_tick = nil
-	state.last_action_type = nil
-end
-
-local function check_action_expired(state, tick)
-	if (state.last_action_tick + TICKS_TO_DETECT_DRAG) < tick then
-		-- stop after x ticks, where no new entity is selected
-		logger.print(player, "stop after x ticks, when no new entity is selected")
-		reset_action(state)
-		return true
-	end
-
-	return false
-end
-
-local function is_last_action(state, type)
-	if not state.last_action_tick or not state.last_action_type then
-		return false
-	end
-	return state.last_action_type[type]
-end
-
-local function handle_action_on_entity(player, selected_entity, state)
+local function handle_action_on_entity(player, selected_entity, state, tick)
 
 	local flying_text_infos = nil
 	if selected_entity.type == "container" or selected_entity.type == "logistic-container" then
 
 		local inventory = selected_entity.get_inventory(defines.inventory.chest)
-		if is_last_action(state, custom_inputs.topupplayerstacks) then
-			set_action(state, game.tick, custom_inputs.topupplayerstacks, true)
-			flying_text_infos = chest.topupplayerstacks(player, inventory)
-		elseif is_last_action(state, custom_inputs.topupentities) then
-			-- stop, when chest is selected
-			reset_action(state)
-			logger.print(player, "stop, when chest is selected")
-		elseif is_last_action(state, custom_inputs.quickstack) then
-			set_action(state, game.tick, custom_inputs.quickstack, true)
-			flying_text_infos = chest.quickstacktoentity(player, inventory)
-		elseif is_last_action(state, custom_inputs.dropitems) then
-			set_action(state, game.tick, custom_inputs.dropitems, true)
-			flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
-		elseif is_last_action(state, custom_inputs.partialstacks) then
-			set_action(state, game.tick, custom_inputs.partialstack, true)
-			flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+		if not player.cursor_stack or not player.cursor_stack.valid_for_read then
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.topupplayerstacks, tick) then
+				flying_text_infos = chest.topupplayerstacks(player, inventory)
+			elseif actiontype.is_last_action(state, custom_inputs.topupentities) then
+				-- stop, when chest is selected
+				actiontype.reset_action(state)
+			elseif actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.quickstack, tick) then
+				flying_text_infos = chest.quickstacktoentity(player, inventory)
+			elseif actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.partialstacks, tick) then
+				flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+			end
+		else
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.dropitems, tick) then
+				flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
+			end
 		end
-
+		
 	elseif selected_entity.type == "ammo-turret" then
 
 		local inventory = selected_entity.get_inventory(defines.inventory.turret_ammo)
-		if is_last_action(state, custom_inputs.topupentities) then
-			set_action(state, game.tick, custom_inputs.topupentities, true)
-			flying_text_infos = turret.topupturret(player, selected_entity, state.setting_max_ammo_amount)
-		elseif is_last_action(state, custom_inputs.dropitems) then
-			set_action(state, game.tick, custom_inputs.dropitems, true)
-			flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
-		elseif is_last_action(state, custom_inputs.partialstacks) then
-			set_action(state, game.tick, custom_inputs.partialstacks, true)
-			flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+		if not player.cursor_stack or not player.cursor_stack.valid_for_read then
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.topupentities, tick) then
+				flying_text_infos = turret.topupturret(player, selected_entity, state.setting_max_ammo_amount)
+			elseif actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.partialstacks, tick) then
+				flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+			end
+		else
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.dropitems, tick) then
+				flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
+			end
 		end
 
 	elseif selected_entity.type == "assembling-machine" then
 		
 		local inventory = selected_entity.get_inventory(defines.inventory.assembling_machine_input)
-		if is_last_action(state, custom_inputs.pickupcraftingslots) then
-			set_action(state, game.tick, custom_inputs.pickupcraftingslots, true)
-			flying_text_infos = assembler.pickupcraftingslots(player, selected_entity)
-		elseif is_last_action(state, custom_inputs.topupentities) then
-			set_action(state, game.tick, custom_inputs.topupentities, true)
-			flying_text_infos = assembler.fillcraftingslots(player, selected_entity)
-		elseif is_last_action(state, custom_inputs.dropitems) then
-			set_action(state, game.tick, custom_inputs.dropitems, true)
-			flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
-		elseif is_last_action(state, custom_inputs.partialstacks) then
-			set_action(state, game.tick, custom_inputs.partialstacks, true)
-			flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+		
+		if not player.cursor_stack or not player.cursor_stack.valid_for_read then
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.pickupcraftingslots, tick) then
+				flying_text_infos = assembler.pickupcraftingslots(player, selected_entity)
+			elseif actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.topupentities, tick) then
+				flying_text_infos = assembler.fillcraftingslots(player, selected_entity)
+			elseif actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.partialstacks, tick) then
+				flying_text_infos = partialstacks.partialstackstoentity(player, inventory)
+			end
+		else
+			if actiontype.is_last_action_if_yes_set_fixed(state, custom_inputs.dropitems, tick) then
+				flying_text_infos = drop.dropitems(player, inventory, state.setting_custom_drop_amount)
+			end
 		end
 
 	end
@@ -139,14 +105,18 @@ local function common_custominput_handler(e)
 	-- while the time period is open, the on_selected_entity_changed eventhandler does its action and extends the period.
 	-- there also some conditions, when the period is closed immediately instead of let x ticks pass by
 
-	set_action(state, game.tick, e.input_name)
+	local tick = game.tick
+	if not actiontype.set_action(state, e.input_name, tick) then
+		logger.print(player, "other fixed action already set")
+		return
+	end
 
 	local selected_entity = player.selected
 	if not selected_entity then
 		return
 	end
 
-	handle_action_on_entity(player, selected_entity, state)
+	handle_action_on_entity(player, selected_entity, state, tick)
 end
 
 script.on_event(custom_inputs.topupplayerstacks, common_custominput_handler)
@@ -168,17 +138,19 @@ script.on_event(defines.events.on_selected_entity_changed, function(e)
 		return
 	end
 
-	if check_action_expired(state, game.tick) then
+	local tick = game.tick
+	if actiontype.check_action_expired(state, tick) then
 		return
 	end
 
 	local selected_entity = player.selected
 	if not selected_entity then
-		-- don't reset, when no entity is selected
+		-- don't reset action, when no entity is selected
+		-- this could be the start of the drag
 		return
 	end
 
-	handle_action_on_entity(player, selected_entity, state)
+	handle_action_on_entity(player, selected_entity, state, tick)
 
 end)
 
@@ -186,7 +158,7 @@ script.on_event(defines.events.on_player_fast_transferred, function(e)
 	-- stop, if something is fast transfered regulary
 	logger.print(e.player_index, "stop, if something is fast transfered regulary")
 	local state = get_player_state(e.player_index)
-	reset_action(state)
+	actiontype.reset_action(state)
 end)
 
 script.on_event(defines.events.on_runtime_mod_setting_changed, function(e)
---
+++ extended-fasttransfer/scripts/actiontype.lua
@@ -0,0 +1,59 @@
+local actiontype = {}
+
+local TICKS_TO_DETECT_DRAG = 30
+
+local logger = require("scripts/logger")
+
+function actiontype.set_action(state, type, tick)
+	if not state.last_action_tick or state.last_action_tick < tick then
+		state.last_action_tick = tick
+    state.last_action_is_fixed = nil
+		state.last_action_types = {}
+		state.last_action_types[type] = true
+		logger.print(player, "set_action: " .. (type or "nil"))
+    return true
+	elseif state.last_action_tick == tick and not state.last_action_is_fixed then
+		state.last_action_types[type] = true
+		logger.print(player, "add_action: " .. (type or "nil"))
+    return true
+	end
+  return false
+end
+
+function actiontype.reset_action(state)
+	state.last_action_tick = nil
+	state.last_action_types = nil
+  state.last_action_is_fixed = nil
+end
+
+function actiontype.check_action_expired(state, tick)
+	if (state.last_action_tick + TICKS_TO_DETECT_DRAG) < tick then
+		-- stop after x ticks, where no new entity is selected
+		logger.print(player, "stop after x ticks, when no new entity is selected")
+		actiontype.reset_action(state)
+		return true
+	end
+
+	return false
+end
+
+function actiontype.is_last_action(state, type)
+	if not state.last_action_tick or not state.last_action_types then
+		return false
+	end
+	return state.last_action_types[type]
+end
+
+function actiontype.is_last_action_if_yes_set_fixed(state, type, tick)
+	if actiontype.is_last_action(state, type) then
+		state.last_action_tick = tick
+		state.last_action_is_fixed = true
+    state.last_action_types = {}
+		state.last_action_types[type] = true
+    logger.print(player, "set_action fixed: " .. (type or "nil"))
+    return true
+	end
+  return false
+end
+
+return actiontype
