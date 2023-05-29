c3826fa90f6919d7423172113fca6ef08de8da1b
jodokus31 <33641866+jodokus31@users.noreply.github.com>
2023-05-29 16:35:09 +0200

    another sorting balance attempt

--- seablock-evil-mode/data-updates/sorting-rebalance.lua
+++ seablock-evil-mode/data-updates/sorting-rebalance.lua
@@ -18,7 +18,7 @@ if not settings.startup['seablock-evil-mode-disable-sorting-rebalance'].value th
     ["angelsore-chunk-mix3-processing"] = {{ name ="bauxite-ore", amount = 3 }},
     ["angelsore-chunk-mix4-processing"] = {{ name ="zinc-ore", amount = 3 }},
     ["angelsore-chunk-mix5-processing"] = {{ name ="silver-ore", amount = 3 }},
-    ["angelsore-chunk-mix6-processing"] = {{ name ="fluorite-ore", amount = 3 }},
+    ["angelsore-chunk-mix6-processing"] = {{ name ="fluorite-ore", amount = 2 }},
     -- ["angelsore-chunk-mix7-processing"] = {{ name ="angels-void", amount = 3 }}, -- disabled
   }
 
@@ -28,14 +28,14 @@ if not settings.startup['seablock-evil-mode-disable-sorting-rebalance'].value th
     ["angelsore-crystal-mix2-processing"] = {{ name ="gold-ore", amount = 4 }},
     ["angelsore-crystal-mix3-processing"] = {{ name ="cobalt-ore", amount = 4 }},
     -- ["angelsore-crystal-mix4-processing"] = {{ name ="angels-void", amount = 3 }}, -- disabled
-    -- ["angelsore-crystal-mix5-processing"] = {{ name ="uranium-ore", amount = 2 }}, -- unchanged
-    -- ["angelsore-crystal-mix6-processing"] = {{ name ="thorium-ore", amount = 2 }}, -- unchanged
+    ["angelsore-crystal-mix5-processing"] = {{ name ="uranium-ore", amount = 2 }},
+    ["angelsore-crystal-mix6-processing"] = {{ name ="thorium-ore", amount = 2 }},
   }
 
   -- Tier 4
   local pure_catalystor_recipes = {
     ["angelsore-pure-mix1-processing"] = {{ name ="tungsten-ore", amount = 4 }},
-    -- ["angelsore-pure-mix2-processing"] = {{ name ="platinum-ore", amount = 2 }}, -- unchanged
+    ["angelsore-pure-mix2-processing"] = {{ name ="platinum-ore", amount = 2 }},
     -- ["angelsore-pure-mix3-processing"] = {{ name ="angels-void", amount = 4 }}, -- disabled
   }
 
@@ -54,12 +54,30 @@ if not settings.startup['seablock-evil-mode-disable-sorting-rebalance'].value th
 
   -- Tier 2
   local chunk_single_ore_recipes = {
-    ["angelsore1-chunk-processing"] = {{ name ="iron-ore", amount = 1}, { name ="quartz", amount = 2}}, -- unchanged
-    ["angelsore2-chunk-processing"] = {{ name ="iron-ore", amount = 1}, { name ="bauxite-ore", amount = 2}},
-    ["angelsore3-chunk-processing"] = {{ name ="copper-ore", amount = 1}, { name ="silver-ore", amount = 2}},
-    ["angelsore4-chunk-processing"] = {{ name ="copper-ore", amount = 1}, { name ="bauxite-ore", amount = 2}},
-    ["angelsore5-chunk-processing"] = {{ name ="lead-ore", amount = 1}, { name ="nickel-ore", amount = 2}},
-    ["angelsore6-chunk-processing"] = {{ name ="tin-ore", amount = 1}, { name ="zinc-ore", amount = 2}},
+    ["angelsore1-chunk-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="quartz", amount = 2}
+    }, -- unchanged
+    ["angelsore2-chunk-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}
+    },
+    ["angelsore3-chunk-processing"] = {
+      { name ="copper-ore", amount = 1}, 
+      { name ="silver-ore", amount = 2}
+    },
+    ["angelsore4-chunk-processing"] = {
+      { name ="copper-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}
+    },
+    ["angelsore5-chunk-processing"] = {
+      { name ="lead-ore", amount = 1}, 
+      { name ="nickel-ore", amount = 2}
+    },
+    ["angelsore6-chunk-processing"] = {
+      { name ="tin-ore", amount = 1}, 
+      { name ="zinc-ore", amount = 2}
+    },
 
     -- ["angelsore8-powder-processing"] = {{ name ="manganese-ore", amount = 3}} }, -- unchanged
     -- ["angelsore9-powder-processing"] = {{ name ="copper-ore", amount = 4}} }, -- unchanged
@@ -68,24 +86,91 @@ if not settings.startup['seablock-evil-mode-disable-sorting-rebalance'].value th
 
   -- Tier 3
   local crystal_single_ore_recipes = {
-    ["angelsore1-crystal-processing"] = {{ name ="iron-ore", amount = 1}, { name ="rutile-ore", amount = 3}},
-    ["angelsore2-crystal-processing"] = {{ name ="iron-ore", amount = 1}, { name ="cobalt-ore", amount = 3}},
-    ["angelsore3-crystal-processing"] = {{ name ="copper-ore", amount = 1}, { name ="uranium-ore", amount = 3}},
-    ["angelsore4-crystal-processing"] = {{ name ="copper-ore", amount = 1}, { name ="bauxite-ore", amount = 3}},
-    ["angelsore5-crystal-processing"] = {{ name ="lead-ore", amount = 1}, { name ="gold-ore", amount = 3}},
-    ["angelsore6-crystal-processing"] = {{ name ="tin-ore", amount = 1}, { name ="silver-ore", amount = 3}},
+    ["angelsore1-crystal-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="quartz", amount = 2}, 
+      { name ="rutile-ore", amount = 2}
+    },
+    ["angelsore2-crystal-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}, 
+      { name ="cobalt-ore", amount = 2}
+    },
+    ["angelsore3-crystal-processing"] = {
+      { name ="copper-ore", amount = 1}, 
+      { name ="silver-ore", amount = 2}, 
+      { name ="uranium-ore", amount = 2}
+    },
+    ["angelsore4-crystal-processing"] = {
+      { name ="copper-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}, 
+      { name ="rutile-ore", amount = 2}
+    },
+    ["angelsore5-crystal-processing"] = {
+      { name ="lead-ore", amount = 1}, 
+      { name ="quartz", amount = 2}, 
+      { name ="gold-ore", amount = 2}
+    },
+    ["angelsore6-crystal-processing"] = {
+      { name ="tin-ore", amount = 1}, 
+      { name ="silver-ore", amount = 2}, 
+      { name ="gold-ore", amount = 2}
+    },
+
     -- ["angelsore8-dust-processing"] = {{ name ="manganese-ore", amount = 4}}, -- unchanged
     -- ["angelsore9-dust-processing"] = {{ name ="silver-ore", amount = 2}, { name ="gold-ore", amount = 2}}, -- unchanged
   }
 
   -- Tier 4
   local pure_single_ore_recipes = {
-    ["angelsore1-pure-processing"] = {{ name ="iron-ore", amount = 1}, { name ="copper-ore", amount = 1}, { name ="tungsten-ore", amount = 3}, { name ="rutile-ore", amount = 3}},
-    ["angelsore2-pure-processing"] = {{ name ="iron-ore", amount = 1}, { name ="copper-ore", amount = 1}, { name ="cobalt-ore", amount = 3}, { name ="tungsten-ore", amount = 3}},
-    ["angelsore3-pure-processing"] = {{ name ="iron-ore", amount = 1}, { name ="copper-ore", amount = 1}, { name ="uranium-ore", amount = 3}, { name ="tungsten-ore", amount = 3}},
-    ["angelsore4-pure-processing"] = {{ name ="iron-ore", amount = 1}, { name ="copper-ore", amount = 1}, { name ="cobalt-ore", amount = 3}, { name ="rutile-ore", amount = 3}},
-    ["angelsore5-pure-processing"] = {{ name ="lead-ore", amount = 1}, { name ="nickel-ore", amount = 1}, { name ="uranium-ore", amount = 3}, { name ="gold-ore", amount = 3}},
-    ["angelsore6-pure-processing"] = {{ name ="tin-ore", amount = 1}, { name ="quartz", amount = 1}, { name ="cobalt-ore", amount = 3}, { name ="gold-ore", amount = 3}},
+    ["angelsore1-pure-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="copper-ore", amount = 1}, 
+      { name ="quartz", amount = 2}, 
+      { name ="nickel-ore", amount = 1}, 
+      { name ="tungsten-ore", amount = 2}, 
+      { name ="rutile-ore", amount = 3}
+    },
+    ["angelsore2-pure-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="copper-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}, 
+      { name ="zinc-ore", amount = 1}, 
+      { name ="cobalt-ore", amount = 2}, 
+      { name ="tungsten-ore", amount = 3}
+    },
+    ["angelsore3-pure-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="copper-ore", amount = 1}, 
+      { name ="silver-ore", amount = 2}, 
+      { name ="tin-ore",  amount = 1}, 
+      { name ="uranium-ore", amount = 3}, 
+      { name ="tungsten-ore", amount = 2}
+    },
+    ["angelsore4-pure-processing"] = {
+      { name ="iron-ore", amount = 1}, 
+      { name ="copper-ore", amount = 1}, 
+      { name ="lead-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}, 
+      { name ="cobalt-ore", amount = 3}, 
+      { name ="rutile-ore", amount = 2}
+    },
+    ["angelsore5-pure-processing"] = {
+      { name ="lead-ore", amount = 1}, 
+      { name ="nickel-ore", amount = 1}, 
+      { name ="bauxite-ore", amount = 2}, 
+      { name ="silver-ore", amount = 1}, 
+      { name ="uranium-ore", amount = 2},
+      { name ="gold-ore", amount = 3}
+      },
+    ["angelsore6-pure-processing"] = {
+      { name ="tin-ore",  amount = 1}, 
+      { name ="quartz", amount = 2}, 
+      { name ="silver-ore", amount = 3}, 
+      { name ="zinc-ore", amount = 1}, 
+      { name ="gold-ore", amount = 2}, 
+      { name ="cobalt-ore", amount = 1}
+    },
     -- angelsore8-crystal-processing-- unchanged
     -- angelsore9-crystal-processing-- unchanged
   }
